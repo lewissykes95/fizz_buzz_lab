ab08ce98c00fb8c4710aca06b7a7cbc862694a08
Lewis <lewisdsykes@gmail.com>
2022-08-03 11:52:42 +0100

    completed the mvp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [currentNumber, setCurrentNumber] = useState(1)
+
+  const [output, setOutput] = useState("")
+
+  const [text, setText] = useState('')
+
+  const [textList, setTextList] = useState([])
+
+  useEffect(() => {
+    console.log('use effect triggered');
+    if (currentNumber % 3 === 0 && currentNumber % 5 === 0) {
+      setOutput("fizzbuzz")
+    } else if (currentNumber % 3 === 0) {
+      setOutput("fizz")
+    } else if (currentNumber % 5 === 0) {
+      setOutput("buzz")
+    } else {
+      setOutput(currentNumber);
+    }
+  }, [currentNumber])
+
+
+  const handleInc = () => {
+    setCurrentNumber(currentNumber + 1)
+  }
+
+  const handleDec = () => {
+    setCurrentNumber(currentNumber - 1)
+  }
+
+  const handleTextChange = (event) => {
+    setText(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const textToSubmit = text.trim();
+    textList.push(textToSubmit)
+    setText("");
+    console.log(textList)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>FizzBuzz</h1>
+      <hr></hr>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={text} onChange={handleTextChange}></input>
+        <input type='submit' value='Add Your Answer'></input>
+      </form>
+      <br></br>
+      <button value={currentNumber} onClick={handleInc}>+</button>
+      <button value={currentNumber} onClick={handleDec}>-</button>
+
+      <h2>{textList}</h2>
+
+      <h2>Number: {currentNumber}</h2>
+
+      <h2>Answer: {output}</h2>
+    </>
+
   );
 }
 
